31904c563d3dcdf972b79fb4d481b9680aeb6feb
avinashagarwal <avinashagarwal@aol.in>
2015-05-07 20:19:01 -0600

    Update FindLongestPalindrome.java

--- src/strings/findlongestpalindrome/FindLongestPalindrome.java
+++ src/strings/findlongestpalindrome/FindLongestPalindrome.java
@@ -1,35 +1,35 @@
-package strings.findlongestpalindrome;
 
-import strings.checkpalindrome.CheckPalindrome;
 
-/**
- * Find longest palindrome string in a given input string.
- * User: rpanjrath
- * Date: 9/18/13
- * Time: 5:53 PM
- */
-public class FindLongestPalindrome {
+public class MyTemp {
+	public static void main(String[] args) {
+		String a = "abcdracecarffeye";
+		char[] arr = a.toCharArray();
+		int length = arr.length;
+		int longest = 0;
+		for(int i=0;i<length;i++) {
+			for(int j=i+1; j < length;j++){
+				if(arr[i] == arr[j]){
+					int itemp = i;
+					int jtemp = j;
+					while(itemp<=jtemp){
+						if(itemp == jtemp){
+							if(j-i+1> longest) {
+								longest = j-i+1;
+							}
+							break;
+						} else
+						if(arr[itemp++] != arr[jtemp--])
+							break;
+					}
+					break;
+				}
+					
+			}
+		}
+		
+		System.out.println("longest " + longest);
+	}
 
-    public static String findLongestPalindrome(String inputString) {
-        if (inputString.isEmpty()) {
-            System.out.print("Invalid input.");
-            return "";
-        }
-        char[] inputStringArray = inputString.toCharArray();
-        String longestPalindrome = "";
-        for (int i = 0; i < inputStringArray.length; i++) {
-            String temp = String.valueOf(inputStringArray[i]);
-            for (int j = i + 1; j < inputStringArray.length; j++) {
-                temp = temp + inputStringArray[j];
-                if(CheckPalindrome.checkPalindromeUsingReversal(temp)) {
-                    if (temp.length() > longestPalindrome.length()) {
-                        longestPalindrome = temp;
-                    }
-                }
-            }
-        }
-        return longestPalindrome;
-    }
-
-    //TODO come up with better solution
 }
+
+//This is in O(n^3) time 
